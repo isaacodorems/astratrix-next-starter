846bb25161aae87aec61fd09e530dfac23c54064
isaacodorems <isaacodo@gmail.com>
2025-11-14 15:36:23 +0100

    Remove next.config.js configuration file

--- next.config.js
+++ next.config.js
@@ -1,12 +1,1 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    formats: ["image/avif", "image/webp"]
-  }
-};
-
-module.exports = nextConfig;
-
-
 
