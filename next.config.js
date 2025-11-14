c71ed28b11c48eae2b5b7f545384c83ecf780e9b
isaacodorems <isaacodo@gmail.com>
2025-11-14 15:33:07 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: { formats: ['image/avif','image/webp'] }
-}
-module.exports = nextConfig
-```
+  images: {
+    formats: ["image/avif", "image/webp"]
+  }
+};
+
+module.exports = nextConfig;
 
 
 
